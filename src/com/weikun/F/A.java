5767cf161c59f99176d98e5bb60d8b2a229ae7c0
weikun <wk2003119@163.com>
2016-12-14 17:00:46 +0800

    1214

--- src/com/weikun/F/A.java
+++ src/com/weikun/F/A.java
@@ -12,6 +12,9 @@ import java.text.SimpleDateFormat;
  * Created by Administrator on 2016/12/5.
  */
 public class A {
+    public static void main(String[] args) {
+
+    }
 
     @Test
     public void copy(){
